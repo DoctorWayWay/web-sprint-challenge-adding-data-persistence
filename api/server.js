ebe6b718ddded91a2165209ed6a6786b36bf7e7e
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-10 07:14:04 -0800

    added required DEP, and server now runs

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
-// build your server here and require it from index.js
+const express = require("express")
+
+const server = express()
+
+server.use(express.json())
+
+// Catch all EP
+server.use("*", (req, res) => {
+  res.status(200).json({ message: "server is working!" })
+})
+
+// Error Handler
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message, // bad for production
+    stack: err.stack,
+  })
+})
+
+module.exports = server
