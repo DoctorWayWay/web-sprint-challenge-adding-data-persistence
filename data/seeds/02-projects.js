a9cdd18adbe076a50567eaf17134cddb3f4cb0b7
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-10 10:02:37 -0800

    fixed seed exports

--- data/seeds/02-projects.js
+++ data/seeds/02-projects.js
@@ -77,8 +77,8 @@ const project_resources = [
 ]
 
 exports.seed = async function (knex) {
-  await knex("recipes").insert(projects)
-  await knex("ingredients").insert(resources)
-  await knex("steps").insert(tasks)
-  await knex("step_ingredients").insert(project_resources)
+  await knex("projects").insert(projects)
+  await knex("resources").insert(resources)
+  await knex("tasks").insert(tasks)
+  await knex("project_resources").insert(project_resources)
 }
